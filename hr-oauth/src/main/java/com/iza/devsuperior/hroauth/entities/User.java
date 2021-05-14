197c164f85b9785e808ca5c3f38695752b8c4589
Izabella Campos <izabellacampos2020@outlook.com>
2021-05-14 18:26:39 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/iza/devsuperior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/iza/devsuperior/hroauth/entities/User.java
@@ -1,9 +1,15 @@
 package com.iza.devsuperior.hroauth.entities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails{
 
 	private Long id;
 	private String name;
@@ -40,6 +46,37 @@ public class User {
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
--- hr-oauth/src/main/java/com/iza/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/iza/devsuperior/hroauth/services/UserService.java
@@ -3,15 +3,18 @@ package com.iza.devsuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.iza.devsuperior.hroauth.entities.User;
 import com.iza.devsuperior.hroauth.feingclients.UserFeingClient;
 
 @Service
-public class UserService {
-	
-	private static Logger logger =  LoggerFactory.getLogger(UserService.class);
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	UserFeingClient userFeingClient;
@@ -22,7 +25,18 @@ public class UserService {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
-		logger.info("Email found: " +email);
+		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeingClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 
