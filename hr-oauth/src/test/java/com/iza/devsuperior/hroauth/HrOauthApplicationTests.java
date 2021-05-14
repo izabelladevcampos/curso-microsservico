119aff8c648f745fadbe6e1256e95946c035345f
Izabella Campos <izabellacampos2020@outlook.com>
2021-05-14 14:59:57 -0300

    Project hr-Oauth created

---
+++ hr-oauth/src/test/java/com/iza/devsuperior/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iza.devsuperior.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
